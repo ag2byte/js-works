d209b2dd41e8c8a4b211e526adeafc9169b975e0
ag2byte1 <capsand123@outlook.com>
2020-12-22 20:31:47 +0530

    changed objects.js

--- intermediate/05objects.js
+++ intermediate/05objects.js
@@ -28,3 +28,30 @@ var obj2 = {
 };
 obj2.isEven(); //[ 3214, 32 ]
 console.log(obj2.evenarr); // [3214,32]
+
+//-------------------------------------------
+// NEW KEYWORD
+// ------------------------------------------
+
+var proto = function (prop1, prop2) {
+  this.prop1 = prop1;
+  this.prop2 = prop2;
+  this.display = function () {
+    console.log(` prop1: ${this.prop1} ,prop2: ${this.prop2}`);
+  };
+};
+
+//here proto is a prototype, similar to what we know as class
+var obj3 = new proto("hello", 69);
+console.log(obj3); // proto { prop1: 'hello', prop2: 69, display: [Function (anonymous)] }
+obj3.display(); // prop1: hello ,prop2: 69
+
+// prototype
+// this is a very handy feature which allows you to add properties to the prototype withpuy going to the prototype definition
+
+proto.prototype.rev = function () {
+  console.log(` prop2: ${this.prop2} ,prop1: ${this.prop1}`);
+};
+
+obj3.rev(); //  prop2: 69 ,prop1: hello
+// more on prototype: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
